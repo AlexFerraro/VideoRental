b44f35df03584670cc0d2d4b9a3e7f999b388777
Alex Ferraro <alex__ferraro@hotmail.com>
2021-03-15 20:24:10 -0300

    Production version.

--- src/javaprova01/FormMain.java
+++ src/javaprova01/FormMain.java
@@ -1,8 +1,6 @@
 
 package javaprova01;
 
-import com.sun.javafx.scene.control.skin.VirtualFlow;
-import java.awt.ComponentOrientation;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -11,6 +9,9 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class FormMain extends javax.swing.JFrame {
     private VideoRental videoRental;
@@ -28,6 +29,7 @@ public class FormMain extends javax.swing.JFrame {
         }     
         
         fillComboBoxMovies();
+        TextAreaSynopsis.setEditable(false);
     }
 
     @SuppressWarnings("unchecked")
@@ -43,6 +45,8 @@ public class FormMain extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         TextAreaSynopsis = new javax.swing.JTextArea();
         jLabel2 = new javax.swing.JLabel();
+        ImagePanel = new javax.swing.JPanel();
+        ImageLable = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -65,12 +69,25 @@ public class FormMain extends javax.swing.JFrame {
 
         jLabel2.setText("Sinopse");
 
+        javax.swing.GroupLayout ImagePanelLayout = new javax.swing.GroupLayout(ImagePanel);
+        ImagePanel.setLayout(ImagePanelLayout);
+        ImagePanelLayout.setHorizontalGroup(
+            ImagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, ImagePanelLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(ImageLable, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        ImagePanelLayout.setVerticalGroup(
+            ImagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(ImageLable, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(41, 41, 41)
                         .addComponent(jLabel1)
@@ -79,15 +96,20 @@ public class FormMain extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(ButtonSelect))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(232, 232, 232)
-                        .addComponent(jLabel2))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addContainerGap()
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 426, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
-                            .addGap(41, 41, 41)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 426, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addContainerGap()
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 426, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                                    .addGap(41, 41, 41)
+                                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 426, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(232, 232, 232)
+                                .addComponent(jLabel2)))
+                        .addGap(73, 73, 73)
+                        .addComponent(ImagePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(14, 14, 14)))
                 .addContainerGap(29, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -99,12 +121,16 @@ public class FormMain extends javax.swing.JFrame {
                     .addComponent(ComboBoxMovies, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(ButtonSelect))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2)
-                .addGap(9, 9, 9)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 351, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel2)
+                        .addGap(9, 9, 9)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 351, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(ImagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -157,24 +183,41 @@ public class FormMain extends javax.swing.JFrame {
             ComboBoxMovies.addItem(movieName);
         }       
     }
-    
+    private String getPathFilmCover(String selectedNameParameter)
+    {
+        File file = new File("..\\VideoRental\\Movie covers\\");
+        File[] fileArray = file.listFiles();
+        String selectedName = selectedNameParameter;
+        String nameHelper = "";
+        
+        for (File arquivo : fileArray) 
+        {
+            nameHelper = arquivo.toString();
+            if(nameHelper.indexOf(selectedName) > -1)
+            {        
+                selectedName = nameHelper;
+                break;
+            }
+        }       
+        return selectedName;
+    }
     private void ButtonSelectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonSelectActionPerformed
         Movie movie = null;
         Assessment highAssessment = null;
         Assessment lowAssessment = null;
-        String SelectedName = null; 
+        String selectedName = null; 
         int movieIndex = -1;
         DefaultListModel model = new DefaultListModel();
         
-        SelectedName = ComboBoxMovies.getSelectedItem().toString();
+        selectedName = ComboBoxMovies.getSelectedItem().toString();
         
-        movieIndex = videoRental.searchMovie(SelectedName);
+        movieIndex = videoRental.searchMovie(selectedName);
                 
         movie = videoRental.getMovies()[movieIndex];
         ListMovie.setModel(model);
         model.addElement("Nome do filme: " + movie.getName());
         model.addElement("Genero: " + movie.getGenre());     
-        model.addElement("Média de avaliações: " + opinions.totalAverageMovieRating(movie.getMovieID()));
+        model.addElement("Média de avaliações: " + String.format("%.2f", opinions.totalAverageMovieRating(movie.getMovieID())));
         
         highAssessment = opinions.highestRatedMovie(movie.getMovieID());
         model.addElement("Nome do Avaliador: " + highAssessment.getEvaluator());
@@ -186,7 +229,9 @@ public class FormMain extends javax.swing.JFrame {
         model.addElement("Comentário do Avaliador: " + lowAssessment.getComment());
         model.addElement("Nota mais baixa: " + lowAssessment.getNote());
         
-        TextAreaSynopsis.setText(movie.getSynopsis());
+        TextAreaSynopsis.setText(movie.getSynopsis());   
+        
+        ImageLable.setIcon(new ImageIcon(getPathFilmCover(selectedName)));
     }//GEN-LAST:event_ButtonSelectActionPerformed
 
     public static void main(String args[]) {
@@ -217,6 +262,8 @@ public class FormMain extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton ButtonSelect;
     private javax.swing.JComboBox<String> ComboBoxMovies;
+    private javax.swing.JLabel ImageLable;
+    private javax.swing.JPanel ImagePanel;
     private javax.swing.JList<String> ListMovie;
     private javax.swing.JTextArea TextAreaSynopsis;
     private javax.swing.JLabel jLabel1;
--- src/javaprova01/JavaProva01.java
+++ src/javaprova01/JavaProva01.java
@@ -1,16 +1,13 @@
 
 package javaprova01;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class JavaProva01 
 {
 
-    public static void main(String[] args) throws FileNotFoundException 
+    public static void main(String[] args)
     {
         FormMain formMain = new FormMain();
+        formMain.setResizable(false);
         formMain.setVisible(true);       
     }   
 }
